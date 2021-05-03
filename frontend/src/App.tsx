62aa8c96c231d0d5b9846bb2f6c5111e47cde493
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-05-03 16:43:40 -0300

    Incluindo os graficos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from "componentes/NavBar";
 import Footer from "componentes/Footer";
 import DataTable from "componentes/DataTable";
+import BarChart from "componentes/BarChart";
+import DonutChart from "componentes/DonutChart";
 
 
 
@@ -9,9 +11,21 @@ function App() {
     <>
        <NavBar/>
       <div className="container">
-        <h1 className="text-primary"> OLá mundo</h1>
+        <h1 className="text-primary py-3"> Dashboard de Vendas</h1>
+        <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+              <BarChart />
+            </div>
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+              <DonutChart />
+            </div>
+            <div className="py-3">
+              <h2 className="text-primary"> Todas as vendas</h2>
+            </div>
+        </div>
         <DataTable />
-      
       </div>
       <Footer />
     </>
