36f8abed69cad731da63266f020a5726567aa77a
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-05-06 12:31:57 -0300

    Criação da camanda controler da seller

--- backend/src/main/java/com/thiagofelipe/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/thiagofelipe/dsvendas/service/SellerService.java
@@ -1,10 +1,12 @@
 package com.thiagofelipe.dsvendas.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.thiagofelipe.dsvendas.dto.SellerDTO;
 import com.thiagofelipe.dsvendas.entidades.Seller;
 import com.thiagofelipe.dsvendas.repositories.SellerRepository;
 
@@ -14,8 +16,9 @@ public class SellerService {
 	@Autowired
 	private SellerRepository repository;
 	
-	public List<Seller> findAll(){
-		return repository.findAll();
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
 		}
 	
 }
