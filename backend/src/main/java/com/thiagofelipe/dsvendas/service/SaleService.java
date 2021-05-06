36be7d740b2d46a51ad6224122baa94df32863c8
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-05-06 16:01:55 -0300

    Resolvendo o problema de várias consultas na paginação das vendas

--- backend/src/main/java/com/thiagofelipe/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/thiagofelipe/dsvendas/service/SaleService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thiagofelipe.dsvendas.dto.SaleDTO;
 import com.thiagofelipe.dsvendas.entidades.Sale;
 import com.thiagofelipe.dsvendas.repositories.SaleRepository;
+import com.thiagofelipe.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -15,9 +17,17 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Autowired
+	private SellerRepository repositorySeller;
+	
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable page){
+		repositorySeller.findAll();
 		Page<Sale> result = repository.findAll(page);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	
 
 }
