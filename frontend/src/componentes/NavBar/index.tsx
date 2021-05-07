76bca94d9f6058483dedc8a86b8b03a3d4c36ddd
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-05-07 14:33:54 -0300

    Routes

--- frontend/src/componentes/NavBar/index.tsx
+++ frontend/src/componentes/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import ImgDsDark from 'assests/img/ds-dark.svg';
+import ImgDsDark from 'assets/img/ds-dark.svg';
 
 function NavBar() {  
     return (
---
+++ frontend/src/pages/Dashbord/index.tsx
@@ -0,0 +1,35 @@
+import NavBar from "componentes/NavBar";
+import Footer from "componentes/Footer";
+import DataTable from "componentes/DataTable";
+import BarChart from "componentes/BarChart";
+import DonutChart from "componentes/DonutChart";
+
+
+
+function Dashboard() {  
+  return (
+    <>
+       <NavBar/>
+      <div className="container">
+        <h1 className="text-primary py-3"> Dashboard de Vendas</h1>
+        <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+              <BarChart />
+            </div>
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Total de Vendas</h5>
+              <DonutChart />
+            </div>
+            <div className="py-3">
+              <h2 className="text-primary"> Todas as vendas</h2>
+            </div>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Dashboard;
